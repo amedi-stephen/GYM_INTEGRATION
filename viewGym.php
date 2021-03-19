16c1dffe56b415aa9d97651f29837d04ca73c1bf
steve <amsteve46@gmail.com>
2021-03-19 12:26:18 +0300

    user UI is almost complete

--- viewGym.php
+++ viewGym.php
@@ -23,100 +23,88 @@ date_default_timezone_set('Africa/Nairobi');
                     </div>
                     <div class="services mt-4">
                     
-                    <div class="service-amenities mb-4">
-                        <h3 class="badge-light p-2">Amenities</h3>';
-            $amenitiesArr = unserialize($gym['amenities']);
-            foreach ($amenitiesArr as $key => $item) {
-                echo '
-                                <ul class="list-group">
-                                <li class="list-group-item">' . $item . '</li> 
-                            </ul>
-                                ';
-            }
+                        <div class="service-amenities mb-4">
+                            <h3 class="badge-light p-2">Amenities</h3>';
+                            $amenitiesArr = unserialize($gym['amenities']);
+                            foreach ($amenitiesArr as $key => $item) {
+                                echo ' <ul class="list-group">
+                                    <li class="list-group-item">' . $item . '</li> 
+                                </ul>';
+                            }
 
-            echo '
-                        
-                    </div>
-                    <div class="service-classes mb-4">
-                        <h3 class="badge-light p-2">Classes</h3>';
-            $classesArr = unserialize($gym['classes']);
-            foreach ($classesArr as $key => $class) {
-                echo '
-                            <ul class="list-group">
-                            <li class="list-group-item">' . $class . '</li> 
-                        </ul>
-                            ';
-            }
-            echo '
-                    </div>
-                    <div class="service-equipments mb-4">
-                        <h3 class="badge-light p-2">Equipments</h3>';
-            $equipArr = unserialize($gym['equipments']);
-            foreach ($equipArr as $key => $equip) {
-                echo '
-                            <ul class="list-group">
-                                <li class="list-group-item">' . $equip . '</li> 
-                            </ul>
-                            ';
-            }
-            echo '
-                    <h3 class="badge-light p-2 mt-4">Other Details</h3>
-                        <ul class="list-group">
-                            <li class="list-group-item d-flex justify-content-between align-items-center">
-                            Opening hours
-                                <span class="badge badge-primary badge-pill"> ' . $gym['opened_at'] . ' - ' . $gym['closed_at'] . '</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center">
-                            Maximum capacity per session
-                                <span class="badge badge-primary badge-pill">' . $gym['full_capacity'] . '</span>
-                            </li>
-                            
-                        </ul>
-                    </div>';
+                        echo '</div>
+                            <div class="service-classes mb-4">
+                            <h3 class="badge-light p-2">Classes</h3>';
+                            $classesArr = unserialize($gym['classes']);
+                            foreach ($classesArr as $key => $class) {
+                                echo '<ul class="list-group">
+                                    <li class="list-group-item">' . $class . '</li> 
+                                </ul>';
+                            }
+                        echo '</div>
+                            <div class="service-equipments mb-4">
+                            <h3 class="badge-light p-2">Equipments</h3>';
+                            $equipArr = unserialize($gym['equipments']);
+                            foreach ($equipArr as $key => $equip) {
+                                echo '<ul class="list-group">
+                                    <li class="list-group-item">' . $equip . '</li> 
+                                </ul>';
+                            }
+                            //TODO: added div here
+                        echo '
+                            <h3 class="badge-light p-2 mt-4">Other Details</h3>
+                                <ul class="list-group">
+                                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                                        Opening hours
+                                        <span class="badge badge-primary badge-pill"> ' . $gym['opened_at'] . ' - ' . $gym['closed_at'] . '</span>
+                                    </li>
+                                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                                        Maximum capacity per session
+                                        <span class="badge badge-primary badge-pill">' . $gym['full_capacity'] . '</span>
+                                    </li>   
+                                </ul>
+                            </div>';
     ?>
             <?php
-            echo "
-                <h3 class='badge-light p-2 mt-4'>Capacity sessions</h3>
-                <div class='float-right'>";
+                        echo "<h3 class='badge-light p-2 mt-4'>Capacity sessions</h3>
+                            <div class='float-right'>";
 
-            $query = 'SELECT * FROM capacity_schedule';
-            $sequel = $DBconnection->query($query);
-            while ($record = $sequel->fetch_assoc()) {
-                //    TODO: how can we make it display only an employers/gym shedule
-                if ($_GET['id']) {
-                    echo "
-                    <div class='card mb-4' style='width: 18rem;'>
-                    <div class='card-body'>
-                    <h5 class='card-title'>" . $record['title'] . "</h5>
-                        <h6 class='card-subtitle mb-2 text-muted'>ksh. " . $record['price'] . "</h6>
-                        <p class='card-text'>" . $record['description'] . "</p>
-                        <form method='post' action='" . reserveCapacity($DBconnection) . "' class='d-inline'>";
-                    $sql = "SELECT * FROM users";
-                    $result = $DBconnection->query($sql);
-                    while ($row = $result->fetch_assoc()) {
-                        if (isset($_SESSION['userID'])) {
-                            // TODO: in resource sessions, put default values of usernames just as this
-                            // TODO: put a date created column in capacity_schedule for users booking
-                            // TODO: add user phone numbers, but for now lets submit the following details
-                            if ($_SESSION['userID'] == $row['user_id']) {
-                                // FIXME: a lot of data redundancy during a single submission
-                                // FIXME: also, when you book the other capacity schedules, it still submits the first id number
-
-                                echo "
-                                        <input type='hidden' name='uid' value='" . $row['user_name'] . "'>
-                                        <input type='hidden' name='date' value='" . date('Y-m-d H:i:s') . "'>
-                                        <button type='submit' name='submit_capacity' class='btn btn-primary'>Book <span class='ml-2'>&#8594;</span></button>
-                                        ";
-                            }
-                        }
-                    }
-                    // TODO: Find a way to calculate if a user books, the number gets added
-                    // ideas include using AJAX for data update without reloading the page
-                    echo "
-                        </form>
-                        <span class='badge badge-pill badge-primary float-right p-2'>1/" . $record['max_pple'] . "</span>
-                    </div>
-                    </div>
+                        $query = 'SELECT * FROM capacity_schedule';
+                        $sequel = $DBconnection->query($query);
+                        while ($record = $sequel->fetch_assoc()) {
+                            // TODO: how can we make it display only an employers/gym shedule
+                            if ($_GET['id']) {
+                                echo "<div class='card mb-4' style='width: 18rem;'>
+                                    <div class='card-body'>
+                                        <h5 class='card-title'>" . $record['title'] . "</h5>
+                                        <h6 class='card-subtitle mb-2 text-muted'>ksh. " . $record['price'] . "</h6>
+                                        <p class='card-text'>" . $record['description'] . "</p>
+                                        <form method='post' action='" . reserveCapacity($DBconnection) . "' class='d-inline'>";
+                                        $sql = "SELECT * FROM users";
+                                        $result = $DBconnection->query($sql);
+                                        while ($row = $result->fetch_assoc()) {
+                                            if (isset($_SESSION['userID'])) {
+                                                // TODO: in resource sessions, put default values of usernames just as this
+                                                // TODO: put a date created column in capacity_schedule for users booking
+                                                // TODO: add user phone numbers, but for now lets submit the following details
+                                                if ($_SESSION['userID'] == $row['user_id']) {
+                                                    // FIXME: a lot of data redundancy during a single submission
+                                                    // FIXME: also, when you book the other capacity schedules, it still submits the first id number
+                                                    echo "
+                                                        <input type='hidden' name='uid' value='" . $row['user_name'] . "'>
+                                                        <input type='hidden' name='date' value='" . date('Y-m-d H:i:s') . "'>
+                                                        <button type='submit' name='submit_capacity' class='btn btn-primary'>Book <span class='ml-2'>&#8594;</span></button>
+                                                        ";
+                                                }
+                                            }
+                                        }
+                                        // TODO: Find a way to calculate if a user books, the number gets added
+                                        // ideas include using AJAX for data update without reloading the page
+                                        echo "
+                                    </form>
+                                    <span class='badge badge-pill badge-primary float-right p-2'>1/" . $record['max_pple'] . "</span>
+                                </div>
+                            </div>
                     </div>";
                 }
             }
@@ -172,54 +160,54 @@ date_default_timezone_set('Africa/Nairobi');
         }
                 ?>
 
-                <div class="comment-section mb-4">
-                    <div class="card" style="width: 30rem">
-                        <div class="card-header">
-                            <h3 class="card-title">Comments</h3>
+                <div class='comment-section mb-4'>
+                    <div class='card' style='width: 30rem'>
+                        <div class='card-header'>
+                            <h3 class='card-title'>Comments</h3>
                         </div>
-                        <div class="card-body">
-                            <ul class="media-list">
-                                <li class="media mb-2">
-                                    <a href="#" class="pull-left">
-                                        <img src="/uploads/caspar-camille-rubin-89xuP-XmyrA-unsplash.jpg" width="45" height="45" class="mr-2">
+                        <div class='card-body'>
+                            <ul class='media-list'>
+                                <li class='media mb-2'>
+                                    <a href='#' class='pull-left'>
+                                        <img src='/uploads/caspar-camille-rubin-89xuP-XmyrA-unsplash.jpg' width='45' height='45' class='mr-2'>
                                     </a>
-                                    <div class="media-body">
+                                    <div class='media-body'>
                                         <p>This Gym equal sucks!</p>
-                                        <strong class="media-heading">
+                                        <strong class='media-heading'>
                                             Steve
                                         </strong>
-                                        <small class="text-muted">A minute ago</small>
+                                        <small class='text-muted'>A minute ago</small>
                                     </div>
                                 </li>
                                 <hr>
                                 <!-- TODO: put appropriate images from the database -->
-                                <li class="media mb-4">
-                                    <a href="#" class="pull-left">
-                                        <img src="images/dummy.jpg" width="45" height="45" class="mr-2">
+                                <li class='media mb-4'>
+                                    <a href='#' class='pull-left'>
+                                        <img src='images/dummy.jpg' width='45' height='45' class='mr-2'>
                                     </a>
-                                    <div class="media-body">
+                                    <div class='media-body'>
                                         <p>Always crowded with no air conditioning</p>
-                                        <strong class="media-heading">
+                                        <strong class='media-heading'>
                                             Jane
                                         </strong>
-                                        <small class="text-muted">A minute ago</small>
+                                        <small class='text-muted'>A minute ago</small>
                                     </div>
                                 </li>
                             </ul>
-                            <form action="" method="post">
-                                <div class="form-group">
-                                    <label for="name"><strong>Name:</strong></label>
-                                    <input type="text" name="name" class="form-control">
+                            <form action='' method='post'>
+                                <div class='form-group'>
+                                    <label for='name'><strong>Name:</strong></label>
+                                    <input type='text' name='name' class='form-control'>
                                 </div>
-                                <div class="form-group">
-                                    <label for="name"><strong>Email:</strong></label>
-                                    <input type="email" name="email" class="form-control">
+                                <div class='form-group'>
+                                    <label for='name'><strong>Email:</strong></label>
+                                    <input type='email' name='email' class='form-control'>
                                 </div>
-                                <div class="form-group">
-                                    <label for="comment"><strong>Comment:</strong></label>
-                                    <textarea name="comment" class="form-control"></textarea>
+                                <div class='form-group'>
+                                    <label for='comment'><strong>Comment:</strong></label>
+                                    <textarea name='comment' class='form-control'></textarea>
                                 </div>
-                                <button class="btn btn-primary mb-4 disabled">Post Comment <i class="fa fa-comment"></i></button>
+                                <button class='btn btn-primary mb-4 disabled'>Post Comment <i class='fa fa-comment'></i></button>
                             </form>
                         </div>
                     </div>
