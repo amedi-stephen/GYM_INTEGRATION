bbb6c4d5ed68a202e12deba071a5a6ee5e7c8d03
steve <amsteve46@gmail.com>
2021-05-16 15:26:16 +0300

    modidfying viewGym.php

--- footer.php
+++ footer.php
@@ -1,10 +1,16 @@
     <div class="container-fluid bg-dark text-center p-2 mt-4 sticky-bottom">
         <p class="text-white text-muted">2021 &#169; Final Year Project. All rights reserved</p>
     </div>
-    
 
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
-    <!-- <script src="js/bootstrap.js"></script> -->
-    <script src="js/jquery-3.5.1.min.js"></script>
-</body>
-</html>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    <script>
+        var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+        var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+            return new bootstrap.Popover(popoverTriggerEl)
+        })
+    </script>
+    </body>
+
+    </html>
--- viewGym.php
+++ viewGym.php
@@ -5,8 +5,8 @@ date_default_timezone_set('Africa/Nairobi');
 ?>
 
 <div class="container mt-4 d-flex flex-column justify-content-center">
-<?php
-    if(isset($_GET['id'])) {
+    <?php
+    if (isset($_GET['id'])) {
         $id = $DBconnection->real_escape_string($_GET['id']);
         $query = "SELECT * FROM gyms WHERE gym_id='$id'";
         $result = $DBconnection->query($query);
@@ -14,71 +14,66 @@ date_default_timezone_set('Africa/Nairobi');
         if ($gym = $result->fetch_assoc()) {
             echo "
             <div class='text-center' style='margin: 20px 0;'>
-                <h2 class='text-center text-uppercase display-4'>".$gym["gym_name"]."</h2>
+                <h2 class='text-center text-uppercase display-4'>" . $gym["gym_name"] . "</h2>
                 <h4>\"Persevearance is the key\"</h4>
-                <h6 class='text-muted'>ksh.2000 per month || ksh.200 per session/day</h6>
+                <h6 class='text-muted'>Email: ".$gym["gym_email"]." || Phone: ".$gym["phone"]."</h6>
             </div>
-            
-            ";
+            <div class='gym-section mb-4'>
+                <div id='slideshow-section' data-component='slideshow'>
+                    <div role='list'>
+                        <div class='slide'>
+                            <img src='images/dark-gym.jpg' />
+                        </div>
+                        <div class='slide'>
+                            <img src='images/dummy.jpg' />
+                        </div>
+                        <div class='slide'>
+                            <img src='images/mountain-climbers.jpg' />
+                        </div>
+                    </div>
+                </div>
+                <div class='gym-features-section'>
+                    <div class='gym-features'>
+                        <h2 class='text-center text-capitalize' style='margin: 20px 0;'>gym features</h2>
+                        <div class='cards-row d-flex flex-wrap justify-content-between'>
+                            <ul class='list-group mb-4' style='width: 20rem;'>
+                            <h2 class='badge-light text-center p-2'>Amenities</h2>";
+                                
+                                $amenitiesArr = unserialize(base64_decode($gym['amenities']));
+                                foreach ($amenitiesArr as $key => $item) {
+                                    echo "
+                                        <li class='list-group-item'>" . $item . "<button type='button' class='btn btn-sm btn-primary float-right' data-bs-toggle='popover' title='Popover title' data-bs-content='And heres some amazing content. Its very engaging. Right?'>View</button></li>";
+                                }
+                            echo "</ul>";
+                            // END OF UNORDERED LIST
+                            echo "<ul class='list-group mb-4' style='width: 20rem'>
+                                    <h2 class='badge-light text-center p-2'>Classes</h2>";
+                                    $classesArr = unserialize(base64_decode($gym['classes']));
+                                    foreach($classesArr as $key => $class) {
+                                        echo "<li class='list-group-item'>" . $class . "<button type='button' class='btn btn-sm btn-primary float-right' data-bs-toggle='popover' title='Popover title' data-bs-content='And heres some amazing content. Its very engaging. Right?'>View</button></li>";
+                                    }
+                            echo "</ul>";
+                            // END OF UNORDERED LIST
+                            echo "<ul class='list-group mb-4' style='width: 20rem'>
+                                    <h2 class='badge-light text-center p-2'>Equipments</h2>";
+                                    $equipArr = unserialize(base64_decode($gym['equipments']));
+                                    foreach($equipArr as $key => $equip) {
+                                        echo "<li class='list-group-item'>" . $equip . "<button type='button' class='btn btn-sm btn-primary float-right' data-bs-toggle='popover' title='Popover title' data-bs-content='And heres some amazing content. Its very engaging. Right?'>View</button></li>";
+                                    }
+                            echo "</ul>";
+                            // END OF UNORDERED LIST
+                        echo "
+                        </div>
+                    </div>
+                </div>";
         }
     }
-?>
+    ?>
     
-    <div class='gym-section mb-4'>
-        <div id='slideshow-section' data-component='slideshow'>
-            <div role='list'>
-                <div class='slide'>
-                    <img src='images/dark-gym.jpg' />
-                </div>
-                <div class='slide'>
-                    <img src='images/dummy.jpg' />
-                </div>
-                <div class='slide'>
-                    <img src='images/mountain-climbers.jpg' />
-                </div>
-            </div>
-        </div>
-        <div class="gym-features-section">
-            <!-- TODO: 
-                *divided into gym features and sessions offered
-             -->
-            <div class="gym-features">
-                <h2 class="text-center text-capitalize" style="margin: 20px 0;">gym features</h2>
-                <div class="cards-row d-flex flex-wrap justify-content-between align-items-center">
-                    <ul class="list-group mb-4" style="width: 20rem;">
-                        <h3 class="badge-light p-2">Ammenities</h3>
-                        <li class="list-group-item">item <i class="fa fa-caret-down"></i></li>
-                        <li class="list-group-item">item <i class="fa fa-caret-down"></i></li>
-                        <li class="list-group-item">item <i class="fa fa-caret-down"></i></li>
-                        <li class="list-group-item">item <i class="fa fa-caret-down"></i></li>
-                    </ul>
-
-                    <ul class="list-group mb-4" style="width: 20rem;">
-                        <h3 class="badge-light p-2">Ammenities</h3>
-                        <li class="list-group-item">item <i class="fa fa-caret-down"></i></li>
-                        <li class="list-group-item">item <i class="fa fa-caret-down"></i></li>
-                        <li class="list-group-item">item <i class="fa fa-caret-down"></i></li>
-                        <li class="list-group-item">item <i class="fa fa-caret-down"></i></li>
-                    </ul>
-
-                    <ul class="list-group mb-4" style="width: 20rem;">
-                        <h3 class="badge-light p-2">Ammenities</h3>
-                        <li class="list-group-item">item <i class="fa fa-caret-down"></i></li>
-                        <li class="list-group-item">item <i class="fa fa-caret-down"></i></li>
-                        <li class="list-group-item">item <i class="fa fa-caret-down"></i></li>
-                        <li class="list-group-item">item <i class="fa fa-caret-down"></i></li>
-                    </ul>
-
-                    <ul class="list-group mb-4" style="width: 20rem;">
-                        <h3 class="badge-light p-2">Ammenities</h3>
-                        <li class="list-group-item">item <i class="fa fa-caret-down"></i></li>
-                        <li class="list-group-item">item <i class="fa fa-caret-down"></i></li>
-                        <li class="list-group-item">item <i class="fa fa-caret-down"></i></li>
-                        <li class="list-group-item">item <i class="fa fa-caret-down"></i></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="gym-sessions"> 
+    
+        <div class='gym-features-section'>
+            
+            <div class="gym-sessions">
                 <h2 class="text-center text-capitalize" style="margin: 20px 0;">gym sessions</h2>
                 <div class="cards-row d-flex flex-wrap justify-content-between align-items-center">
                     <div class="card" style="width: 18rem;">
@@ -115,6 +110,14 @@ date_default_timezone_set('Africa/Nairobi');
 </div>
 
 
+
+
+
+
+
+
+
+
 <div class="container mt-4 d-flex flex-column justify-content-center">
     <?php
     if (isset($_GET['id'])) {
@@ -132,40 +135,40 @@ date_default_timezone_set('Africa/Nairobi');
                     
                         <div class="service-amenities mb-4">
                             <h3 class="badge-light p-2">Amenities</h3>';
-                            $amenitiesArr = unserialize(base64_decode($gym['amenities']));
-                            foreach ($amenitiesArr as $key => $item) {
-                                echo ' <ul class="list-group">
+            $amenitiesArr = unserialize(base64_decode($gym['amenities']));
+            foreach ($amenitiesArr as $key => $item) {
+                echo ' <ul class="list-group">
                                     <li class="list-group-item">' . $item . '</li> 
                                 </ul>';
-                            }
+            }
 
-                            echo '
+            echo '
                         </div>
 
                         <div class="service-classes mb-4">
                             <h3 class="badge-light p-2">Classes</h3>';
-                                $classesArr = unserialize(base64_decode($gym['classes']));
-                                if(is_array($classesArr) || is_object($classesArr)) {
-                                    foreach($classesArr as $class) {
-                                        echo "<ul class='list-group'>
-                                            <li class='list-group-item'>".$class."</li>
+            $classesArr = unserialize(base64_decode($gym['classes']));
+            if (is_array($classesArr) || is_object($classesArr)) {
+                foreach ($classesArr as $class) {
+                    echo "<ul class='list-group'>
+                                            <li class='list-group-item'>" . $class . "</li>
                                         </ul>";
-                                    }
-                                } else {
-                                    echo "Array conversion error: ".$DBconnection->error;
-                                }
-                            echo '
+                }
+            } else {
+                echo "Array conversion error: " . $DBconnection->error;
+            }
+            echo '
                         </div>
 
                         <div class="service-equipments mb-4">
                             <h3 class="badge-light p-2">Equipments</h3>';
-                            $equipArr = unserialize(base64_decode($gym['equipments']));
-                            foreach ($equipArr as $key => $equip) {
-                                echo '<ul class="list-group">
+            $equipArr = unserialize(base64_decode($gym['equipments']));
+            foreach ($equipArr as $key => $equip) {
+                echo '<ul class="list-group">
                                         <li class="list-group-item">' . $equip . '</li> 
                                 </ul>';
-                            }
-                            echo '
+            }
+            echo '
                         </div>
 
                         <div class="service-others mb-4">
@@ -185,14 +188,14 @@ date_default_timezone_set('Africa/Nairobi');
             <?php
             echo " <div class='capacity-sessions'>
                 <h3 class='badge-light p-2 mt-4'>Capacity sessions</h3>";
-                
-                if (isset($_GET['id'])) {
+
+            if (isset($_GET['id'])) {
                 $sql = "SELECT * FROM gyms";
                 $result = $DBconnection->query($sql) or die($DBconnection->error);
-                while($row = $result->fetch_assoc()) {
+                while ($row = $result->fetch_assoc()) {
                     $gymPage = $_GET['id'];
                     $gymid = $row['gym_id'];
-                    if($gymid == $gymPage) {
+                    if ($gymid == $gymPage) {
                         $query = "SELECT * FROM capacity_schedule WHERE gym_id = '$gymPage'";
                         $sequel = $DBconnection->query($query);
                         while ($record = $sequel->fetch_assoc()) {
@@ -202,21 +205,21 @@ date_default_timezone_set('Africa/Nairobi');
                                         <h6 class='card-subtitle mb-2 text-muted'>ksh. " . $record['price'] . "</h6>
                                         <p class='card-text'>" . $record['description'] . "</p>
                                         <form method='post' action='" . reserveCapacity($DBconnection) . "' class='d-inline'>";
-                                            $sql = "SELECT * FROM users";
-                                            $result = $DBconnection->query($sql);
-                                            while ($row = $result->fetch_assoc()) {
-                                                if (isset($_SESSION['userID'])) {
-                                                    if ($_SESSION['userID'] == $row['user_id']) {
-                                                        echo "
+                            $sql = "SELECT * FROM users";
+                            $result = $DBconnection->query($sql);
+                            while ($row = $result->fetch_assoc()) {
+                                if (isset($_SESSION['userID'])) {
+                                    if ($_SESSION['userID'] == $row['user_id']) {
+                                        echo "
                                                             <input type='hidden' name='uid' value='" . $row['user_name'] . "'>
                                                             <input type='hidden' name='date' value='" . date('Y-m-d H:i:s') . "'>
                                                             <button type='submit' name='submit_capacity' class='btn btn-primary btn-sm'>Book <span class='ml-2'>&#8594;</span></button>";
-                                                    }
-                                                }
-                                            }
-                                        echo "</form>";
-                                            echo "
-                                            <a href='userScheduleSess.php?sessID=".$record['capacity_id']."' class='btn btn-sm btn-outline-primary float-right'>More</a>
+                                    }
+                                }
+                            }
+                            echo "</form>";
+                            echo "
+                                            <a href='userScheduleSess.php?sessID=" . $record['capacity_id'] . "' class='btn btn-sm btn-outline-primary float-right'>More</a>
                                     </div>
                                 </div>
                                 </div>
@@ -225,24 +228,28 @@ date_default_timezone_set('Africa/Nairobi');
                     }
                 }
             }
-      
+
             ?>
 
             <div class='container-fluid mt-4'>
-                
+
                 <table class='table mt-4'>
-                    
+
                     <tbody>
                 <?php
-                
-               
-    }
+
+
+            }
         }
                 ?>
 
-                
+
             </div>
 </div>
+
+<?php
+    require "footer.php";
+?>
 
 
 <script>
@@ -251,16 +258,15 @@ date_default_timezone_set('Africa/Nairobi');
 
     function initSlideShow(slideshow) {
         const slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
-        let index = 0, time = 3000;
+        let index = 0,
+            time = 5000;
         slides[index].classList.add("active");
 
         setInterval(() => {
             slides[index].classList.remove("active");
             index++;
-            if(index === slides.length) index = 0;
+            if (index === slides.length) index = 0;
             slides[index].classList.add("active");
         }, time);
     }
 </script>
-
-
