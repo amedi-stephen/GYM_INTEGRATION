fd9cc25acd50affb17d879b298ae3d6bc8046d77
steveamedi <amsteve46@gmail.com>
2021-05-18 11:57:36 +0300

    gym admin to see the bookings (closed)

--- employee/viewSession.php
+++ employee/viewSession.php
@@ -26,28 +26,89 @@ include '../includes/processSchedule.inc.php';
                         
                     // }
                     
-                    $query = "SELECT * FROM capacity_schedule WHERE capacity_id = '$sessPage'";
+                    $query = "SELECT * FROM capacity_schedule";
                     $sequel = $DBconnection->query($query) or die("Error occurred: " . $DBconnection->error);
+                    $record = $sequel->fetch_assoc();
+                
+                    echo '<span>'.$sessPage.'</span>';
+                    echo '<span>'.$gymid.'</span>';
+                    $sqlMembers = "SELECT * FROM capacity_members WHERE gym_id = '$gymid' AND capacity_id = '$sessPage' ";
+                    $resultMembers = $DBconnection->query($sqlMembers) or die("Error occurred: " . $DBconnection->error);
+                    $resultRows = $resultMembers->num_rows;
                     
-                    while ($record = $sequel->fetch_assoc()) {
-                        $sqlMembers = "SELECT * FROM capacity_members WHERE gym_id = '$gymid'";
-                        $resultMembers = $DBconnection->query($sqlMembers) or die("Error occurred: " . $DBconnection->error);
-                        $resultRows = $resultMembers->num_rows;
-                        if ($member = $resultMembers->fetch_assoc()) {
-                            echo "<div class='card mb-4 mt-4'>
-                            <div class='card-header' style='background-color:blue'>
-                                <h5 clas='card-title'>" . $record['title'] . "</h5>
-                                <h6 class='text-muted'>" . $record['price'] . "</h6>
+                    while($my_res =  $resultMembers->fetch_assoc()){
+                        echo "<div class='card mb-4 mt-4'>
+
+                            <div class='card-header flex' style='background-color:blue;color:white;'>
+                                <h5 clas='card-title'>" . $my_res['username'] . "</h5>
                             </div>
                             <div class='card-body'>
-                                <p>" . $record['description'] . "</p>
-                                <p class='d-inline badge-primary p-1'>" . $resultRows . " Member(s) have booked your session</p>
-                                <p class='float-right text-muted'>" . $record['from_date'] . "</p>
+                            <h6 style='color:white'>" . $my_res['amount_paid'] . "</h6>
+                                <p>" . $my_res['email'] . "</p>
+
+                                <p> total " . $resultRows . "</p>
+                                <p> total session id " . $sessPage . "</p>
+                                <p> total " . $record['capacity_id'] . "</p>
+
                             </div>
-                            
+
+                        
                         </div>";
-                        }
                     }
+                    
+                   
+                    // if( $sessPage ){
+                    //     echo '<h1>'.$sessPage.'</h1>';
+                    //     $sqlMembers = "SELECT * FROM capacity_members WHERE gym_id = '$gymid'";
+                    //     $resultMembers = $DBconnection->query($sqlMembers) or die("Error occurred: " . $DBconnection->error);
+                    //     $resultRows = $resultMembers->num_rows;
+                        
+                    //     while($my_res =  $resultMembers->fetch_assoc()){
+                    //         echo "<div class='card mb-4 mt-4'>
+
+                    //          <div class='card-header flex' style='background-color:blue;color:white;'>
+                    //              <h5 clas='card-title'>" . $my_res['username'] . "</h5>
+                    //          </div>
+                    //          <div class='card-body'>
+                    //             <h6 style='color:white'>" . $my_res['amount_paid'] . "</h6>
+                    //              <p>" . $my_res['email'] . "</p>
+
+                    //              <p> total " . $resultRows . "</p>
+                    //              <p> total session id " . $sessPage . "</p>
+                    //              <p> total " . $record['capacity_id'] . "</p>
+
+                    //          </div>
+
+                            
+                    //      </div>";
+                    //     }
+                    // }
+
+                    
+                    
+                    // while ($record = $sequel->fetch_assoc()) {
+                    //     echo '<pre>';
+                    //     var_dump($record);
+                    //     echo '</pre>';
+                    //     $sqlMembers = "SELECT * FROM capacity_members WHERE gym_id = '$gymid'";
+                    //     $resultMembers = $DBconnection->query($sqlMembers) or die("Error occurred: " . $DBconnection->error);
+                    //     $resultRows = $resultMembers->num_rows;
+                    //     echo 'hello motto';
+                    //     if ($member = $resultMembers->fetch_assoc()) {
+                    //         echo "<div class='card mb-4 mt-4'>
+                    //         <div class='card-header' style='background-color:blue'>
+                    //             <h5 clas='card-title'>" . $record['title'] . "</h5>
+                    //             <h6 class='text-muted'>" . $record['price'] . "</h6>
+                    //         </div>
+                    //         <div class='card-body'>
+                    //             <p>" . $record['description'] . "</p>
+                    //             <p class='d-inline badge-primary p-1'>" . $resultRows . " Member(s) have booked your session</p>
+                    //             <p class='float-right text-muted'>" . $record['from_date'] . "</p>
+                    //         </div>
+                            
+                    //     </div>";
+                    //     }
+                    // }
                     // $capacityID = $row['capacity_members_id'];
                 }
             } else {
